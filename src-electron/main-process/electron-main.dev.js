54614da3a19bd50da9a0fa61f9e2bd968b311558
Egortsev Daniil Dmitrievich <degortsev@ozon.ru>
2021-01-14 01:28:24 +0300

    chore: add electron

---
+++ src-electron/main-process/electron-main.dev.js
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/**
+ * This file is used specifically and only for development. It installs
+ * `electron-debug` & `vue-devtools`. There shouldn't be any need to
+ *  modify this file, but it can be used to extend your development
+ *  environment.
+ */
+
+import electronDebug from 'electron-debug';
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
+import { app, BrowserWindow } from 'electron';
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const startApp = async () => {
+  await app.whenReady();
+  // allow for a small delay for mainWindow to be created
+  await sleep(250);
+  // Install `electron-debug` with `devtron`
+  electronDebug({ showDevTools: false });
+
+  // Install vuejs devtools
+  try {
+    const name = await installExtension(VUEJS_DEVTOOLS);
+    console.log(`Added Extension: ${name}`);
+    // get main window
+    const win = BrowserWindow.getFocusedWindow();
+    if (win) {
+      win.webContents.on('did-frame-finish-load', () => {
+        win.webContents.once('devtools-opened', () => {
+          win.webContents.focus();
+        });
+        // open electron debug
+        console.log('Opening dev tools');
+        win.webContents.openDevTools();
+      });
+    }
+  } catch (err) {
+    console.error('An error occurred: ', err);
+  }
+};
+
+startApp().catch((e) => console.error('Error to start app:', e));
+
+import './electron-main';
---
+++ src-electron/main-process/electron-main.js
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import { app, BrowserWindow, nativeTheme } from 'electron';
+import fs from 'fs';
+import path from 'path';
+
+try {
+  if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
+    fs.unlinkSync(path.join(app.getPath('userData'), 'DevTools Extensions'));
+  }
+} catch (_) {
+  throw new Error(`Error: ${_}`);
+}
+
+/**
+ * Set `__statics` path to static files in production;
+ * The reason we are setting it here is that the path needs to be evaluated at runtime
+ */
+if (process.env.PROD) {
+  global.__statics = __dirname;
+}
+
+let mainWindow;
+
+function createWindow() {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    useContentSize: true,
+    webPreferences: {
+      // Change from /quasar.conf.js > electron > nodeIntegration;
+      // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
+      nodeIntegration: process.env.QUASAR_NODE_INTEGRATION,
+      nodeIntegrationInWorker: process.env.QUASAR_NODE_INTEGRATION,
+
+      // More info: /quasar-cli/developing-electron-apps/electron-preload-script
+      // preload: path.resolve(__dirname, 'electron-preload.js')
+    },
+  });
+
+  mainWindow.loadURL(process.env.APP_URL);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
